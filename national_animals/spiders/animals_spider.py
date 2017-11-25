8414308a579597c5acd98c821175e5a3a0a43971
Adam <adam@adamharpur.com>
2017-11-25 11:47:14 -0600

    dev(list development):still off

--- national_animals/spiders/animals_spider.py
+++ national_animals/spiders/animals_spider.py
@@ -31,7 +31,9 @@ class AnimalsSpider(scrapy.Spider):
         data_list = []
         country_list = []
         animal_list = []
-        print("OUR DATA")
+        sci_name_list = []
+        pic_list = []
+
         # arr = []
         # print(titles)
         # print("-------------------->>>>>")
@@ -43,49 +45,83 @@ class AnimalsSpider(scrapy.Spider):
              if(country):
                  country_list.append(country)
 
-             #extract animal and attach animal to list 
+             # extract animal and attach animal to list 
              animal = data.css('td>a:first-child::attr(title)').extract_first()
              if(animal):
                     animal_list.append(animal)
+
+             # scientific name 
+             sci_name = data.css('td>i:only-child::text').extract_first()
+
+             if(sci_name):
+                 sci_name_list.append(sci_name)
+             else:
+                 sci_name_list.append("mythical")
+             
+             # picture
+             pic = data.css('td>a>img::attr(src)').extract()
+             
+             if(pic):
+                 pic_list.append(pic)
+             else:
+                 pic_list.append("none")
+        
+
              # check for rowspans
              n_row = data.css('td::attr(rowspan)').extract_first()
              if n_row:
                     title_map.extend(n_row)
              else:
                     title_map.extend('0')
-        # pprint.pprint(title_map)
+        
         # chunked = list(self.chunks(title_map,4))
         # pprint.pprint(chunked)
 
         # print(country_list)
         # print(animal_list)
+        # print(title_map)
         i = 0
         counter = 0
+        del sci_name_list[0]
+        del sci_name_list[0]
+        del pic_list[0]
+        del pic_list[0]
         # manually remove Kurdistan :(
         del country_list[41]
-        while counter < len(country_list):
+        while counter < len(animal_list):
             item = Item()
-            rowspan = title_map[i + 2]
+            if(i < len(title_map) - 2):
+                rowspan = title_map[i + 2]
             item['animal'] = []
-            item['country'] = country_list[counter]
+            item['sci_name'] = []
+            item['pic'] = []
+            if counter < len(country_list):
+                item['country'] = country_list[counter]
+
             if int(rowspan) > 0:
                 count = 0
-                if i < len(country_list):
+                if i < len(animal_list):
                     while count < int(rowspan):
                         item['animal'].append(animal_list[i + count])
-                        print(count)
-                        print("------------------")
+                        item['sci_name'].append(sci_name_list[i + count])
+                        item['pic'].append(pic_list[i + count])
                         count += 1
                     i = i + count - 1
             else:
-                item['animal'].append(animal_list[i])
-            print(i)
+                if i < len(animal_list):
+                    item['animal'].append(animal_list[i])
+                    item['sci_name'].append(sci_name_list[i])
+                    item['pic'].append(pic_list[i])
             data_list.append(item)
             i += 1
             counter += 1 
         
         pprint.pprint(data_list)
-
+        
+        file = open('tester.json', 'w')
+        for i in data_list:
+            file.write("%s\n" % i)
+     
 
         ## NOT IN USE CURRENTLY
         # for i in range(len(title_map) + 1):
