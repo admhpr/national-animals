8a7bd8066a86d3dfd7706458c3d559c16bc5f0bd
Adam <adam@adamharpur.com>
2017-11-28 09:49:22 -0600

    task(joined geo data and animal data)

--- web_scraper/data_joiner.py
+++ web_scraper/data_joiner.py
@@ -1,7 +1,7 @@
 # the idea here is to take the scraped national data and add it as 
 # props of the world geo data set.
 # source of world data set: https://github.com/johan/world.geo.json
-
+from itertools import zip_longest
 import json
 import pprint
 
@@ -10,12 +10,24 @@ with open('world.geo.json', encoding='utf-8') as data_file:
     world_data = json.loads(data_file.read())
 
 with open('national_animals.json', encoding='utf-8') as data_file:
-    national_animals_data = json.loads(data_file.read())
+    national_animals_list = json.loads(data_file.read())
 
-# world data = 180
-# national animals data = 80 
-pprint.pprint(len(world_data['features']))
-pprint.pprint(len(national_animals_data))
+animals_dict = {"name" : "national_animals", "data" : national_animals_list}
 
-# for d in world_data['features']:
-#     pprint.pprint(d['id'])
+# ranges
+# world data: 180
+# national animals data: 80 
+# matchable: 72
+
+i = 0
+while i < len(world_data['features']):
+    world_data['features'][i]['properties']['country'] = world_data['features'][i]['properties'].pop('name')
+    for d in range(len(animals_dict['data'])):
+        if(animals_dict['data'][d]["_values"]['country'] == world_data['features'][i]['properties']['country']):
+            world_data['features'][i]['properties']['national_animal']= animals_dict['data'][d]["_values"]['animal']
+            world_data['features'][i]['properties']['sci_name']= animals_dict['data'][d]["_values"]['sci_name']
+            world_data['features'][i]['properties']['media']= animals_dict['data'][d]["_values"]['pic'] 
+    i += 1
+
+with open('national_animals_map.json', 'w') as outfile:
+    json.dump(world_data, outfile, indent = 2)
